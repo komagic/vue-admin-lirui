ba3c45b50da8b1b4c9a33871bcbf8e7d2b88db13
Jacky <910367438@qq.com>
2023-03-28 13:32:51 +0800

    fix: proxy

--- src/cool/config/proxy.ts
+++ src/cool/config/proxy.ts
@@ -1,12 +1,12 @@
 export const proxy = {
 	"/dev/": {
-		target: "http://150.158.147.122:8001",
+		target: "http://150.158.147.122:8002",
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/dev/, ""),
 	},
 
 	"/prod/": {
-		target: "http://150.158.147.122:8001",
+		target: "http://150.158.147.122:8002",
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/prod/, "")
 	},
